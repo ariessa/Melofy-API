ba58886b8ea7ce6780072e0e5baae01d9e5105d7
Ariessa <ariessa@users.noreply.github.com>
2021-05-20 00:40:42 +0800

    Cleaned code

--- app.py
+++ app.py
@@ -15,12 +15,23 @@ from note_seq.protobuf import music_pb2
 from timidity import Parser, play_notes
 from scipy.signal import square, sawtooth
 from scipy.io import wavfile
+from google.cloud import storage
 
 # Create a Flask application object
 app = Flask(__name__)
 
+# Explicitly use service account credentials by specifying the private key file
+storage_client = storage.Client.from_service_account_json('melofy-1b47c-33e8fbb61207.json')
+
 # Start the debugger
 app.config["DEBUG"] = True
+
+# Initialize the basic_rnn model from Magenta
+bundle = sequence_generator_bundle.read_bundle_file('basic_rnn.mag')
+generator_map = melody_rnn_sequence_generator.get_generator_map()
+melody_rnn = generator_map['basic_rnn'](checkpoint=None, bundle=bundle)
+melody_rnn.initialize()
+print("\n\nInitialized the basic_rnn model from Magenta")
 
 # A route to return the homepage
 @app.route('/', methods=['GET'])
@@ -36,7 +47,6 @@ def page_not_found(e):
 # A route to generate melody from supplied audio file link
 @app.route('/generate', methods=['POST'])
 def generate_melody():
-    # TODO: Get storage_account, input_container_name, output_container_name
     # If there is no request data in json or audio_file item in request data
     # Abort the request and return error code 400
     if not request.json or not 'audio_file_link' in request.json:
@@ -44,54 +54,48 @@ def generate_melody():
 
     # Retrieve audio file link from request.json
     audio_file_link = request.json['audio_file_link']
-    # audio_file_link = "https://melofyapi.blob.core.windows.net/melofy-api-input/twinkle_twinkle_little_star.wav"
     print("\n\naudio_file_link: " + audio_file_link)
-    # working
 
     # Extract audio file name from audio_file_link
     audioFileName = audio_file_link.split('/')[-1]
     print("\n\nExtracted audio file name from audio_file_link")
     print("\naudioFileName: " + audioFileName)
-    # working
 
     # Path to store audio file
     # Get current working directory
     # This is the path where you run the flask app
     pathToAudioFile = os.getcwd()
     print("\n\nCurrent working directory: " + pathToAudioFile)
-    # working
+
+    # Append input folder to end of path to store audio file
+    inputFolder = "input"
+    pathToAudioFile = pathToAudioFile + "\\" + inputFolder
 
     # Azure's Blob Connection string
     connectionString = "DefaultEndpointsProtocol=https;AccountName=melofyapi;AccountKey=ucqmEO03FyTs7/z9JS7pQkA7bIai7O0ycBs09Iataco8xk3BcxRDNmy5+NqYLqYjiMxTP8ZRRsaHFT4HRvn0Dw==;EndpointSuffix=core.windows.net"
-    # working
 
     try:
         # Create the BlobServiceClient object which will be used to get blob
         blob_service_client = BlobServiceClient.from_connection_string(connectionString)
         print("\n\nCreated the BlobServiceClient object which will be used to get blob")
-        # working
 
         # Set the container name
         input_container_name = "melofy-api-input"
         print("\n\nSet the container name")
         print("\n\ncontainer_name: " + input_container_name)
-        # working
 
         # Create a blob client using the audioFileName as the name for the blob
         blob_client = blob_service_client.get_blob_client(container=input_container_name, blob=audioFileName)
         print("\n\nCreated a blob client using the audioFileName as the name for the blob")
-        # working
 
         # Set path to store download file
         download_file_path = os.path.join(pathToAudioFile, audioFileName)
         print("\n\nDownloading blob to \n\t" + download_file_path)
-        # working
 
         # Download the blob to pathToAudioFile
         with open(download_file_path, "wb") as download_file:
             download_file.write(blob_client.download_blob().readall())
         print("\n\nDownloaded the blob to \n\t" + download_file_path)
-        # working
 
     except Exception as ex:
         print('Exception:')
@@ -101,40 +105,26 @@ def generate_melody():
     # Extract all characters before . in audioFileName
     head, sep, tail = audioFileName.partition('.')
     print("\n\nExtracted all characters before . in audioFileName")
-    # working
 
     # Concatenate filename with .mid extension
     convertedAudioFileName = head + ".mid"
     print("\n\nConcatenated filename with .mid extension")
     print("\nconvertedAudioFileName: " + convertedAudioFileName)
-    # working
 
     # Command to convert WAV file to MIDI file
-    command = "audio-to-midi --output " + convertedAudioFileName + " " + audioFileName
-    # working
+    command = "audio-to-midi --output " + inputFolder + "\\" + convertedAudioFileName + " " + inputFolder + "\\" + audioFileName 
 
     # Execute the command
     stream = os.popen(command)
-    # working
 
     # Display output of command
     output = stream.readlines()
-    # working
 
     print("\n\nConverted WAV file to MIDI file")
 
     # Convert MIDI file to Note Sequence file
-    audioNoteSequence = note_seq.midi_file_to_note_sequence(convertedAudioFileName)
+    audioNoteSequence = note_seq.midi_file_to_note_sequence(inputFolder + "\\" + convertedAudioFileName)
     print("\n\nConverted MIDI file to Note Sequence file")
-    # working
-
-    # Initialize the basic_rnn model from Magenta
-    bundle = sequence_generator_bundle.read_bundle_file('basic_rnn.mag')
-    generator_map = melody_rnn_sequence_generator.get_generator_map()
-    melody_rnn = generator_map['basic_rnn'](checkpoint=None, bundle=bundle)
-    melody_rnn.initialize()
-    print("\n\nInitialized the basic_rnn model from Magenta")
-    # working
 
     # Model options
     input_sequence = audioNoteSequence
@@ -157,102 +147,68 @@ def generate_melody():
     # Ask the model to continue the sequence
     sequence = melody_rnn.generate(input_sequence, generator_options)
     print("\n\nGenerated melody from audio file")
-    # working
 
     # Generate file name for generated melody (MIDI)
     # Extract all characters before . in convertedAudioFileName
     head, sep, tail = convertedAudioFileName.partition('.')
     print("\n\nExtracted all characters before . in convertedAudioFileName")
-    # working
 
     # Concatenate file name for generated melody with prefix gm- and .mid extension
     generatedMelody = "gm-" + head + ".mid"
     print("\n\nConcatenate file name for generated melody with prefix gm- and .mid extension")
     print("\ngeneratedMelody: " + generatedMelody)
-    # working
 
     # Convert Note Sequence file to MIDI file
-    note_seq.sequence_proto_to_midi_file(sequence, generatedMelody)
+    note_seq.sequence_proto_to_midi_file(sequence, inputFolder + "\\" + generatedMelody)
     print("\n\nConverted Note Sequence file to MIDI file")
-    # working
 
     # Currently, no conversion from MIDI to WAV file will be executed
     # This is because the WAV file produces a high pitch sound that's not favourable to ears
-    # # Generate file name for generated melody (WAV)
-    # # Extract all characters before . in convertedAudioFileName
-    # head, sep, tail = generatedMelody.partition('.')
-    # print("\n\nExtracted all characters before . in generatedMelody")
-    # # working
+    # Generate file name for generated melody (WAV)
+    # Extract all characters before . in convertedAudioFileName
+    head, sep, tail = generatedMelody.partition('.')
+    print("\n\nExtracted all characters before . in generatedMelody")
 
-    # # Concatenate file name for generated melody with .wav extension
-    # convertedGeneratedMelody = head + ".wav"
-    # print("\n\nConcatenate file name for generated melody with .wav extension")
-    # print("\ngeneratedMelody: " + convertedGeneratedMelody)
-    # # working
+    # Concatenate file name for generated melody with .wav extension
+    convertedGeneratedMelody = head + ".wav"
+    print("\n\nConcatenate file name for generated melody with .wav extension")
+    print("\ngeneratedMelody: " + convertedGeneratedMelody)
 
-    # # Convert MIDI file to WAV file
-    # ps = Parser(generatedMelody)
-    # audio, player = play_notes(*ps.parse(), sawtooth, wait_done=False)
-    # wavfile.write(convertedGeneratedMelody, 44100, audio)
-    # print("\n\nConverted MIDI file to WAV file")
-    # working
+    # Convert MIDI file to WAV file
+    ps = Parser(inputFolder + "\\" + generatedMelody)
+    audio, player = play_notes(*ps.parse(), sawtooth, wait_done=False)
+    wavfile.write(inputFolder + "\\" + convertedGeneratedMelody, 44100, audio)
+    print("\n\nConverted MIDI file to WAV file")
 
-    # Upload generatedMelody to Azure Blob Storage Container
+    # Upload generatedMelody to Firebase Cloud Storage
     try:
-        # Create the BlobServiceClient object which will be used to upload blob
-        blob_service_client = BlobServiceClient.from_connection_string(connectionString)
-        print("\n\nCreated the BlobServiceClient object which will be used to get blob")
-        # working
+        # The ID of your Google Cloud S bucket
+        bucket_name = "melofy-1b47c.appspot.com"
 
-        # Set the container name
-        output_container_name = "melofy-api-output"
-        print("\n\nSet the container name")
-        print("\n\ncontainer_name: " + output_container_name)
-        # working
+        # The path to your file to upload
+        source_file_name = inputFolder + "\\" + convertedGeneratedMelody
 
-        # Create a blob client using the convertedGeneratedMelody as the name for the blob
-        blob_client = blob_service_client.get_blob_client(container=output_container_name, blob=generatedMelody)
-        print("\n\nCreated a blob client using the convertedGeneratedMelody as the name for the blob")
-        # working
+        # The ID of your GCS object
+        destination_blob_name = convertedGeneratedMelody
 
-        # Set path of file to upload
-        upload_file_path = os.path.join(pathToAudioFile, generatedMelody)
-        # working
+        bucket = storage_client.bucket(bucket_name)
+        blob = bucket.blob(destination_blob_name)
 
-        # Create a blob client using the local file name as the name for the blob
-        blob_client = blob_service_client.get_blob_client(container=output_container_name, blob=generatedMelody)
-        print("\n\nUploading to Azure Storage as blob:\n\t" + generatedMelody)
-        # working
+        blob.upload_from_filename(source_file_name)
 
-        # Upload the created file
-        with open(upload_file_path, "rb") as data:
-            blob_client.upload_blob(data)
-        print("\n\nUploaded to Azure Storage as blob:\n\t" + generatedMelody)
-        # working
+        print(
+            "File {} uploaded to {}.".format(
+                source_file_name, destination_blob_name
+            )
+        )
 
     except Exception as ex:
         print('Exception:')
         print(ex)
 
-    # Set link to uploaded audio file in Azure Blob Storage
-    generatedMelodyLink = "https://melofyapi.blob.core.windows.net/" + output_container_name + "/" + generatedMelody
-
-    return generatedMelodyLink
+    return convertedGeneratedMelody
 
 # Run the application server
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host = '0.0.0.0')
-
-# Send POST request using curl on Windows
-# curl -i -X POST -H "Content-Type:application/json" -d "{\"<name-of-item>\": \"<item>\" }" http://<domain>/<route>
-
-# Example of correct POST request using curl on Windows
-# curl -i -X POST -H "Content-Type:application/json" -d "{\"audio_file_link\": \"https://melofyapi.blob.core.windows.net/melofy-api-input/twinkle_twinkle_little_star.wav\" }" http://localhost:5000/generate
-# curl -i -X POST -H "Content-Type:application/json" -d "{\"audio_file_link\": \"https://melofyapi.blob.core.windows.net/melofy-api-input/twinkle_twinkle_little_star.mid\" }" http://melofyapi.ap.ngrok.io/generate
-
-# Start ngrok
-# ngrok http -region=<region> -hostname=<subdomain>.<region>.ngrok.io <port>
-
-# Example of starting ngrok
-# ngrok http -region=ap -hostname=melofyapi.ap.ngrok.io 5000
